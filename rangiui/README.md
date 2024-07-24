2d22b308865d6507b051c540e55576684cdc0384
Collins Omwenga <comwenga@live.com>
2024-07-24 03:49:29 +0300

    chore: Remove unused files and update dependencies

--- rangiui/README.md
+++ rangiui/README.md
@@ -1,4 +1,4 @@
-# RangiUI
+# rangiui
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- rangiui/vite.config.ts
+++ rangiui/vite.config.ts
@@ -3,15 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx()],
+  build: {
+    outDir: 'dist/vue-app'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
